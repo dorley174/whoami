1f0e49a0258637d1f4e8b6bbf9b946540daa294e
dorley174 <d.valiev@innopolis.university>
2025-07-04 17:52:26 +0300

    feat: added start page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from "../css/styles.module.css"
 
 const HomePage: React.FC = () => {
   return (
     <main>
-      <h1>Добро пожаловать в игру Who Am I</h1>
-      <p>Это главная страница.</p>
-      <Link href="/rules">
-        Правила игры
-      </Link>
+      <div className={styles.headertitle}>
+        <div className={styles.headersubtitle}>
+          <div className={styles.headerspan}>
+            <span className={styles.block}>Guess.</span>
+            <span className={styles.block}>Play.</span>
+            <span className={styles.block}>Enjoy.</span>
+          </div>
+          <Link href="/menu" className={styles.playbutton}>
+            <p>Start</p>
+          </Link>
+        </div>
+        <div className={styles.headerdesc}>
+          <p><strong>Who Am I</strong>  isn’t just a game—it’s an explosion of emotions where every word becomes the key to success. It’s a battle of quick thinking, wit, and teamwork that turns any gathering into an unforgettable experience.</p>
+        </div> 
+        
+      </div>
+      
     </main>
   );
 };
