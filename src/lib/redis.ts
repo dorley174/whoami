b5ae472195402f30f04d9f9abf4534a11cf8e69b
dorley174 <d.valiev@innopolis.university>
2025-07-08 23:21:03 +0300

    fix: removed .vscode from gitignore, logging api errors

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,5 +1,9 @@
 import { Redis } from '@upstash/redis';
 
+console.log('Redis URL:', process.env.UPSTASH_REDIS_REST_URL);
+console.log('Redis Token:', process.env.UPSTASH_REDIS_REST_TOKEN);
+
+
 export const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL!,
   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
@@ -11,21 +15,26 @@ const TTL_SECONDS = 60 * 60 * 12;
 // Сохранение комнаты
 export async function setRoom(code: string, data: { players: string[] }) {
   try {
+    console.log(`Setting room with code: ${code}`);
     await redis.set(`room:${code}`, data, { ex: TTL_SECONDS });
+    console.log(`Room with code ${code} saved successfully`);
   } catch (err) {
     console.error(`Error setting room ${code}:`, err);
     throw new Error('Failed to set room');
   }
 }
 
+
 // Проверка существования комнаты
 export async function isRoomExists(code: string): Promise<boolean> {
   try {
+    console.log(`Checking if room exists: ${code}`);
     const room = await redis.get(`room:${code}`);
+    console.log(`Room exists check for ${code}:`, room ? 'Exists' : 'Does not exist');
     return room !== null;
   } catch (err) {
     console.error(`Error checking if room exists ${code}:`, err);
-    return false; // Можем вернуть false, чтобы не останавливать выполнение
+    return false;
   }
 }
 
