1b128725eea4a9909ca4c4b46350ceeafc661d32
dorley174 <d.valiev@innopolis.university>
2025-07-08 04:41:54 +0300

    fix: fix api folder placement due vercel and 405 error 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../css/globals.css';
-import HeaderPage from '../components/header';
-import FooterPage from '../components/footer';
+import HeaderPage from '../src/components/header';
+import FooterPage from '../src/components/footer';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
